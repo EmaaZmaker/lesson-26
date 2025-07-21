3148e4213dc52dcbfd5a89db73daadd678ce7903
EmaaZmaker <emaazbinammar@gmail.com>
2025-07-21 11:59:57 +0400

    ef

--- apy.py
+++ apy.py
@@ -1,11 +1,5 @@
-numbers1=[1,2,3]
-numbers2=[4,5,6]
-result=map(lambda x,y:x+y,numbers1,numbers2)
-print(list(result))
-
-#using map
-list1=[1,2,3,4,5]
-def square (n):
+def sq_n(n):
     return n*n
-list2=list(map(square,list1))
-print(list2)
+list1=[1,2,3,4,5]
+new_list=map(sq_n,list1)
+print(list(new_list))
